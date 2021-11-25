9234097a1db2ee3cd6976c70b44e3050369207c2
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2021-11-25 10:15:28 +0700

    Added SignUp() and PilihanBuku()
    Been a long time since last commit. Though it's not finished yet.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
 import gui.SignIn;
 
-/**
- *
- * @author haris
- */
 public class Main {
     /**
      * @param args the command line arguments
@@ -38,7 +29,7 @@ public class Main {
             java.util.logging.Logger.getLogger(SignIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
